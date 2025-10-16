49d049f0064dc98e16d02cae5fcade220a341199
Ramiro Sanabria <ramineu@icloud.com>
2025-10-16 16:24:18 -0300

    Cambios.

--- Unidad 6/6.10.c
+++ Unidad 6/6.10.c
@@ -9,24 +9,60 @@
 // 2 Reste un dia a la fecha dada.
 // 3 Sume n dias a la fecha dada.
 // 4 Reste n dias a la fecha dada.
+// 5 que salga
+#include <stdio.h>
 
-#include <stdio>
-
-
-
-int esFechaVal();
+int esFechaVal(int dia, int mes, int anio);
 int CantDiasMes(int m, int a);
 int esBisiesto(int a);
+int calcularDia(int dia, int mes, int anio, int numero);
+int mostrarMenuValorElegido();
+int sumaDia(int dia, int mes, int anio);
+int restaDia(int dia, int mes, int anio);
+int sumaNDia(int dia, int mes, int anio, int n);
+int restaNDia(int dia, int mes, int anio, int n);
 int main(){
     
+    int num, dia, mes, anio, n;
+    
+    do {
+        printf("Introduce una fecha valida\n");
+        
+        printf("\nDia: ");
+        scanf("%d", &dia);
+        
+        printf("\nMes: ");
+        scanf("%d", &mes);
+        
+        printf("\nAño: ");
+        scanf("%d", &anio);
+        
+    }while (esFechaVal(dia, mes, anio) == 0);
+        
+    do {
+        switch (num = mostrarMenuValorElegido()) {
+            case 1: sumaDia(dia, mes, anio); break;
+            case 2: restaDia(dia, mes, anio); break;
+            case 3: printf("\nIngrese cantidad de dias: ");
+                    scanf("%d", &n);
+                    sumaNDia(dia, mes, anio, n); break;
+            case 4: printf("\nIngrese cantidad de dias: ");
+                    scanf("%d", &n);
+                    restaNDia(dia, mes, anio, n); break;
+            case 5: printf("\n\nBye"); break;
+            }
+        
+    } while (num != 5);
+        
 }
 
-int esFechaVal(int dia, int mes, int anio)
-
-{
-if (anio > 1900)
+int esFechaVal(int dia, int mes, int anio){
+if (anio > 1600)
     if (mes >= 1 && mes <= 12)
         if (dia >= 1 && dia <= CantDiasMes(mes, anio))
+            return 1;
+    
+    return 0;
 }
 
 int CantDiasMes(int m, int a){
@@ -47,4 +83,113 @@ int esBisiesto(int a){
     else
         bis=0;
     return bis;
+}
+
+
+int sumaDia(int dia, int mes, int anio){
+    int df, mf, af;
+    
+    df = dia + 1;
+    mf = mes;
+    af = anio;
+    
+    if (df > CantDiasMes(mes, anio)){
+        df -=CantDiasMes(mes, anio);
+        mf++;
+        if (mf > 12){
+            mf = 1;
+            af++;        }
+    }
+    printf ("%d/%d/%d mas un dia es: %d/%d/%d", dia, mes, anio, df, mf, af);
+    return 0;
+}
+
+int restaDia(int dia, int mes, int anio){
+    int df, mf, af;
+    
+    df = dia - 1;
+    mf = mes;
+    af = anio;
+    
+    if (df == 0){
+        mf--;
+        if (mf == 0){
+            mf = 12;
+            af--;
+        }
+        df = CantDiasMes(mf, af);
+    }
+    
+printf("%d/%d/%d menos un dia es: %d/%d/%d", dia, mes, anio, df, mf, af);
+    return 0;
+}
+
+
+int mostrarMenuValorElegido(){
+    
+    int num;
+    printf("==========================\n");
+    printf("Menu de opciones:\n");
+    printf("==========================\n");
+    printf("\n1) Suma 1 dia\n");
+    printf("\n2) Resta 1 dia\n");
+    printf("\n3) Sumar N numeros\n");
+    printf("\n4) Restar N numeros\n");
+    printf("\n5) Salir\n");
+    printf("==========================\n");
+    
+    do {
+        printf("Ingrese la opcion: ");
+        scanf("%d", &num);
+    } while (num < 0 || num > 6);
+    
+    return num;
+}
+
+
+int sumaNDia(int dia, int mes, int anio, int n){
+    int df, mf, af;
+    
+    df = dia + n;
+    mf = mes;
+    af = anio;
+    
+    do {
+        
+        if (df > CantDiasMes(mes, anio)){
+            df -=CantDiasMes(mes, anio);
+            mf++;
+            if (mf > 12){
+                mf = 1;
+                af++;
+            }
+        }
+        
+    }while (esFechaVal(dia, mes, anio)== 0);
+    
+    printf ("\n%d/%d/%d mas un dia es: %d/%d/%d\n", dia, mes, anio, df, mf, af);
+    return 0;
+}
+
+
+int restaNDia(int dia, int mes, int anio, int n){
+    int df, mf, af;
+    
+    df = dia - n; //-99
+    mf = mes; //1
+    af = anio; //2000
+    
+    do {
+        if (df < 1){ // -99 == 0
+            mf--;
+            if (mf == 0){
+                mf = 12;
+                af--;
+            }
+            df = CantDiasMes(mf, af);
+        }
+    } while (mf != 13);
+    
+printf("\n%d/%d/%d menos un dia es: %d/%d/%d\n", dia, mes, anio, df, mf, af);
+    return 0;
 }
